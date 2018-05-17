97bd6ef612308986f3e3079f3e331d25924a7d69
Triton Wolfe <triton.wolfe@gmail.com>
2018-05-17 09:34:37 -0500

    add some imports and the start method

--- Treasurer/FinanceApp.java
+++ Treasurer/FinanceApp.java
@@ -1,5 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ListView;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Vbox;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 
 public class FinanceApp extends Application {
 
+    @Override
+    public void start(Stage stage) {
+
+    }
 }
