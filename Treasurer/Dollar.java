4c6a617524a4bbbed2a6cbc4efe59f1cb8ab5d70
Triton Wolfe <triton.wolfe@gmail.com>
2018-05-17 08:56:56 -0500

    add charge and deposit methods as well as a default cents value constructor

--- Treasurer/Dollar.java
+++ Treasurer/Dollar.java
@@ -1,16 +1,46 @@
 public class Dollar {
     private int cents;
     private int dollars;
+    private boolean inDebt;
 
+    public Dollar(int dollars) {
+        this(dollars, 0);
+    }
     public Dollar(int dollars, int cents) {
         this.cents = cents % 100;
         this.dollars = dollars + (cents / 100);
     }
 
-    public void add(Dollar other) {
+    public void deposit(Dollar other) {
         this.cents = (this.cents + other.getCents) % 100;
         this.dollars += other.getDollars;
         this.dollars += (this.cents + other.getCents) / 100;
+    }
+
+    public void deposit(int dollars) {
+        this.deposit(dollars, 0);
+    }
+
+    public void deposit(int dollars, int cents) {
+        this.cents = (this.cents + cents) % 100;
+        this.dollars += dollars;
+        this.dollars += (this.cents + cents) / 100;
+    }
+
+    public void charge(Dollar other) {
+        this.cents = (this.cents - other.getCents) % 100;
+        this.dollars -= other.getDollars;
+        this.dollars -= (this.cents - other.getCents) / 100;
+    }
+
+    public void charge(int dollars) {
+        this.charge(dollars, 0);
+    }
+
+    public void charge(int dollars, int cents) {
+        this.cents = (this.cents - cents) % 100;
+        this.dollars -= dollars;
+        this.dollars -= (this.cents - cents) / 100;
     }
 
     public int getDollars() {
