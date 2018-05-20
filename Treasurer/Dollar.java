a53c1dddd04a97b09508ff3f949cece55ee754e8
triton-wolfe <triton.wolfe@gmail.com>
2018-05-20 12:19:18 -0500

    add functional methods to Dollar and Person

--- Treasurer/Dollar.java
+++ Treasurer/Dollar.java
@@ -1,70 +1,34 @@
 public class Dollar {
-    private int cents;
-    private int dollars;
+    private double dollars;
 
-    public Dollar(int dollars) {
-        this(dollars, 0);
-    }
-    public Dollar(int dollars, int cents) {
-        this.cents = cents % 100;
-        this.dollars = dollars + (cents / 100);
+    public Dollar(double dollars) {
+        this.dollars = dollars;
     }
 
-    public void deposit(Dollar other) {
-        this.cents = (this.cents + other.getCents) % 100;
-        this.dollars += other.getDollars;
-        this.dollars += (this.cents + other.getCents) / 100;
-    }
-a
-    public void deposit(int dollars) {
-        this.deposit(dollars, 0);
-    }
-
-    public void deposit(int dollars, int cents) {
-        this.cents = (this.cents + cents) % 100;
+    public void deposit(double dollars) {
         this.dollars += dollars;
-        this.dollars += (this.cents + cents) / 100;
     }
 
-    public void charge(Dollar other) {
-        this.cents = (this.cents - other.getCents) % 100;
-        this.dollars -= other.getDollars;
-        this.dollars -= (this.cents - other.getCents) / 100;
-    }
-
-    public void charge(int dollars) {
-        this.charge(dollars, 0);
-    }
-
-    public void charge(int dollars, int cents) {
-        this.cents = (this.cents - cents) % 100;
+    public void charge(double dollars) {
         this.dollars -= dollars;
-        this.dollars -= (this.cents - cents) / 100;
-    }
-
-    public int getDollars() {
-        return this.dollars;
-    }
-
-    public int getCents() {
-        return this.dollars;
     }
 
     @Override
     public int hashCode() {
-        return 100*dollars + cents;
+        return (int) (100 * this.dollars);
     }
 
     @Override
     public boolean equals(Object other) {
         if (this == other) { return true; }
-        if (!other instanceof dollar) { return false; }
+        if (!(other instanceof Dollar)) { return false; }
         Dollar o = (Dollar) other;
-        return this.toString == o.toString;
+        return this.toString().equals(o.toString());
     }
 
     @Override
     public String toString() {
-        return "$" + this.dollars + "." + this.cents;
+        return String.format("$%.2f",this.dollars);
     }
+
 }
--- Treasurer/Person.java
+++ Treasurer/Person.java
@@ -1,3 +1,55 @@
-public abstract class Person {
+public class Person {
+    private String nickname;
+    private String fullname;
+    private Dollar balance;
 
+    public Person(String name) {
+        this(name, name, new Dollar(0))
+    }
+
+    public Person(String name, double balance) {
+        this(name, name, new Dollar(balance));
+    }
+
+    public Person(String nickname, String fullname) {
+        this(nickname, fullname, new Dollar(0));
+    }
+
+    public Person(String nickname, String fullname, double balance) {
+        this(nickname, fullname, new Dollar(balance))
+    }
+
+    public Person(String nickname, String fullname, Dollar balance) {
+        this.nickname = nickname;
+        this.fullname = fullname;
+        this.balance = balance;
+    }
+
+    public String getNickname() {
+        return this.name;
+    }
+
+    public Dollar getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * this.balance.hashCode()
+            + 31 * this.name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) { return true; }
+        if (!(other instanceof Person)) { return false; }
+        Person o = (Person) other;
+        return this.name.equals(o.getName())
+            && this.balance.equals(o.getBalance());
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": Balance " + this.balance.toString();
+    }
 }
