f6a0bbc8e723254cc500f7ebd5a368985961856d
Triton Wolfe <triton.wolfe@gmail.com>
2018-05-29 13:04:29 -0500

    add project class to workplanner

---
+++ WorkPlanner/Project.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Project {
+    private String name;
+    private String classification;
+    private ArrayList<WorkItems> workItems;
+
+    public Project(String name, String classification) {
+        this.name = name
+        this.classification = classification;
+        this.workItems = new ArrayList<>();
+    }
+
+    public String getName() { return this.name; }
+    public String getClassification() { return this.classification; }
+    public ArrayList<WorkItems> getWorkItems() { return this.workItems; }
+
+    public String toJSON() {
+        String workItemJson = "";
+        for (WorkItem w: workItems) {
+            workItemJson += w.toJSON();
+        }
+        return String.format(
+            "Project {%nname: %s%nclassification: %s%nworkItems: %s%n}%n",
+            this.name, this.classification, workItemJson);
+    }
+}
