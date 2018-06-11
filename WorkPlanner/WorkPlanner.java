bd1d9dc532d323ab8da67e92d47987ae89b755b5
Triton Wolfe <twolfe@scriptpro.com>
2018-06-11 13:16:50 -0500

    remove unneeded import

--- WorkPlanner/WorkPlanner.java
+++ WorkPlanner/WorkPlanner.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
